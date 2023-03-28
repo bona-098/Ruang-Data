3b41dcf145edf61596c5e0dc2b314b96d5a79d14
bona-098 <pencengirm@gmail.com>
2023-03-28 10:32:11 +0800

    be karyawan

---
+++ app/Http/Controllers/KaryawanController.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\karyawan;
+use Illuminate\Http\Request;
+
+class KaryawanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $karya = karyawan::get();
+        return view ('xx.xy');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'nik' => 'required',
+            'nama_karyawan' => 'required',
+            'tgl_lahir'=> 'required',
+            'kota_lahir'=> 'required',
+            'agama'=> 'required',
+            'jenis_kelamin'=> 'required',
+            'jalan'=> 'required',
+            'nama_status_nikah'=> 'required',
+            'lokasi_kerja'=> 'required',
+            'tgl_mulai_bekerja'=> 'required',
+            'tgl_pegawai_perusahaan'=> 'required',
+            'tgl_perkiraan_pensiun'=> 'required',
+            'nama_divisi'=> 'required',
+            'tgl_divisi'=> 'required',
+            'kode_loker'=> 'required',
+            'unit_kerja'=> 'required',
+            'loker'=> 'required',
+            'tgl_loker'=> 'required',
+            'kode_posisi'=> 'required',
+            'jabatan'=> 'required',
+            'umur'=> 'required',
+            'kelompok_usia'=> 'required',
+            'nama_employee_group'=> 'required',
+            'employee_sub_group'=> 'required',
+            'level_pendidikan_terakhir'=> 'required',
+            'pendidikan'=> 'required',
+            'penyelenggara'=> 'required',
+            'jumlah_anak'=> 'required',
+            'adt_pajak'=> 'required',
+            'adt_kesehatan'=> 'required',
+            'jumlah_pasangan'=> 'required',
+            'band_kelas_posisi'=> 'required',
+            'kota'=> 'required',
+            'npwp'=> 'required',
+            'no_telkomedika'=> 'required',
+            'no_bpjs'=> 'required',
+            'no_jamsostek'=> 'required',
+            'email'=> 'required',
+            'no_hp'=> 'required',
+            'keterangan_lanjut_kuliah'=> 'required',
+            'pendidikan_lanjut'=> 'required',
+            'penyelenggara_lanjut'=> 'required',
+            'ukuran_baju'=> 'required'
+        ]);
+
+        Karyawan::create([
+            'nik'=>$request->nik,
+            'nama_karyawan'=>$request->nama_karyawan,
+            'tgl_lahir'=>$request->tgl_lahir,
+            'kota_lahir'=>$request->kota_lahir,
+            'agama'=>$request->agama,
+            'jenis_kelamin'=>$request->jenis_kelamin,
+            'jalan'=>$request->jalan,
+            'nama_status_nikah'=>$request->nama_status_nikah,
+            'lokasi_kerja'=>$request->lokasi_kerja,
+            'tgl_mulai_bekerja'=>$request->tgl_mulai_bekerja,
+            'tgl_pegawai_perusahaan'=>$request->tgl_pegawai_perusahaan,
+            'tgl_perkiraan_pensiun'=>$request->tgl_perkiraan_pensiun,
+            'nama_divisi'=>$request->nama_divisi,
+            'tgl_divisi'=>$request->tgl_divisi,
+            'kode_loker'=>$request->kode_loker,
+            'unit_kerja'=>$request->unit_kerja,
+            'loker'=>$request->loker,
+            'tgl_loker'=>$request->tgl_loker,
+            'kode_posisi'=>$request->kode_posisi,
+            'jabatan'=>$request->jabatan,
+            'umur'=>$request->umur,
+            'kelompok_usia'=>$request->kelompok_usia,
+            'nama_employee_group'=>$request->nama_employee_group,
+            'employee_sub_group'=>$request->employee_sub_group,
+            'level_pendidikan_terakhir'=>$request->level_pendidikan_terakhir,
+            'pendidikan'=>$request->pendidikan,
+            'penyelenggara'=>$request->penyelenggara,
+            'jumlah_anak'=>$request->jumlah_anak,
+            'adt_pajak'=>$request->adt_pajak,
+            'adt_kesehatan'=>$request->adt_kesehatan,
+            'jumlah_pasangan'=>$request->jumlah_pasangan,
+            'band_kelas_posisi'=>$request->band_kelas_posisi,
+            'kota'=>$request->kota,
+            'npwp'=>$request->npwp,
+            'no_telkomedika'=>$request->no_telkomedika,
+            'no_bpjs'=>$request->no_bpjs,
+            'no_jamsostek'=>$request->no_jamsostek,
+            'email'=>$request->email,
+            'no_hp'=>$request->no_hp,
+            'keterangan_lanjut_kuliah'=>$request->keterangan_lanjut_kuliah,
+            'pendidikan_lanjut'=>$request->pendidikan_lanjut,
+            'penyelenggara_lanjut'=>$request->penyelenggara_lanjut,
+            'ukuran_baju'=>$request->ukuran_baju
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_03_27_022231_create_perangkat_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perangkat', function (Blueprint $table) {
+            $table->id();
+            $table->string('no');
+            $table->string('id_group');
+            $table->string('id_area');
+            $table->string('id_unit');
+            $table->string('nama_unit');
+            $table->string('id_witel');
+            $table->string('nama_witel');
+            $table->string('id_location');
+            $table->string('nama_lokasi');
+            $table->string('id_gedung');
+            $table->string('nama_gedung');
+            $table->string('id_kelas');
+            $table->string('id_room');
+            $table->string('id_lantai');
+            $table->string('nama_lantai');
+            $table->string('jid');
+            $table->string('nama_jenis');
+            $table->string('kid');
+            $table->string('kategori');
+            $table->string('skid');
+            $table->string('sub_kategori');
+            $table->string('nama_perangkat');
+            $table->string('is_ceklis');
+            $table->string('merk');
+            $table->string('satuan');
+            $table->string('jumlah');
+            $table->string('kapasitas')->nullable();
+            $table->string('no_seri')->nullable();
+            $table->string('type')->nullable();
+            $table->string('tahun')->nullable();
+            $table->string('kondisi')->nullable();
+            $table->string('milik')->nullable();
+            $table->string('keterangan')->nullable();
+            $table->string('id_perangkat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perangkat');
+    }
+};
